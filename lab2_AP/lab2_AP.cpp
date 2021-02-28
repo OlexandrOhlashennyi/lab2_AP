995646a069ee726dc6fdfe70b5cae0fc5b29b1a3
OlexandrOhlashennyi <oleksandr.ohlashennyi.knm.2020@lpnu.ua>
2021-02-28 22:21:45 +0200

    coments

--- lab2_AP/lab2_AP.cpp
+++ lab2_AP/lab2_AP.cpp
@@ -5,12 +5,28 @@
 
 using namespace std;
 
-void getDate(char s[], int n) {
-	int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	char montharr[][10] = { "січня", "лютого", "березня", "квітня", "травня", "червня", "липня", "серпня", "вересня", "жовтня", "листопада", "грудня" };
-	if (n < 1 || n > 365) {
+char* getDate(char[], int);					//прототип ф-ії отримання дати за номером дня
+char* concat(char[], int, const char*);		//прототип ф-ії об'єднання рядків
+
+int main() {
+	system("chcp 1251"); system("cls");
+	int n;
+	char s[13];
+	do {
+		printf("Введіть номер дня року(нуль -- кінець): ");
+		scanf("%d", &n);
+		puts(getDate(s, n));
+		getchar();
+	} while (n != 0);
+	return 0;
+}
+
+char* getDate(char s[], int n) {
+	int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };		//кількості днів у місяцях
+	const char* montharr[] = { "січня", "лютого", "березня", "квітня", "травня", "червня", "липня", "серпня", "вересня", "жовтня", "листопада", "грудня" }; //назви місяців
+	if (n < 1 || n > 365) { //перевірка правильності введення
 		strcpy(s, "");
-		return;
+		return s;
 	}
 	int month = 0, days = 0, i = 0;
 	while (days + daysInMonth[i] < n) {
@@ -18,21 +34,13 @@ void getDate(char s[], int n) {
 		month++;
 	}
 	days = n - days;
-	_itoa(days, s, 10);
-	strcat(s, " ");
-	strcat(s, montharr[month]);
+	concat(s, days, montharr[month]); //об'єднання назви місяця і дня
+	return s;
 }
 
-int main() {
-	system("chcp 1251"); system("cls");
-	int n;
-	char s[12];
-	do {
-		printf("Введіть номер дня року(нуль -- кінець): ");
-		scanf("%d", &n);
-		getDate(s, n);
-		puts(s);
-		getchar();
-	} while (n != 0);
-	return 0;
+char* concat(char s[], int a, const char *month) {
+	_itoa(a, s, 10);
+	strcat(s, " ");
+	strcat(s, month);
+	return s;
 }
